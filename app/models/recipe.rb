014126be0f8bc85161689641c3166fd470a5b62f
Vitaliy <vitaliy.fry@gmail.com>
2016-03-21 00:43:50 +0300

    add image for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
 
+  has_attached_file :image, styles: { large: "500x500" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
   validates :chef_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
   validates :summary, presence: true, length: { minimum: 30, maximum: 300 }
