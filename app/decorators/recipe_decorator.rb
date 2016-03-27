e2cbd8fa3aec2b5f74bece97b6dbe27062584746
Vitaliy <vitaliy.fry@gmail.com>
2016-03-27 04:24:58 +0300

    add actions for owner

--- app/decorators/recipe_decorator.rb
+++ app/decorators/recipe_decorator.rb
@@ -2,8 +2,21 @@ class RecipeDecorator < Draper::Decorator
   delegate_all
 
   include ActionView::Helpers::DateHelper
+  include Rails.application.routes.url_helpers
 
   def posted_by
     "Posted by: #{time_ago_in_words(object.created_at)} ago"
+  end
+
+  def check_edit_action(user_id)
+    if object.chef_id == user_id
+      h.link_to 'Edit', edit_recipe_path(object.id), class: 'btn btn-warning'
+    end
+  end
+
+  def check_destroy_action(user_id)
+    if object.chef_id == user_id
+      h.link_to 'Delete', recipe_path(object.id), method: :delete, class: 'btn btn-danger'
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
 # Destroy all
 Recipe.destroy_all
+User.destroy_all
 Chef.destroy_all
 
 # Generate fake data
 5.times do 
-  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
-end
-
-5.times do |count|
-  Chef.create!(name: Faker::Name.name, avatar: Faker::Avatar.image, user_id: count+1)
-end
-
-5.times do
+  user = User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
   Recipe.create!(name: Faker::Lorem.word, summary: Faker::Lorem.paragraph, description: Faker::Lorem.paragraph(15),
-                 chef_id: Chef.order('RANDOM()').first.id, image: Faker::Avatar.image)
+                 chef_id: user.id, image: Faker::Avatar.image)
 end
