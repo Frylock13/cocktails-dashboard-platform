28d6425bb5b342a7102e915050fce2705b00b1fe
Vitaliy <vitaliy.fry@gmail.com>
2016-03-29 04:17:22 +0300

    refactor chef sql query

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,15 @@
 class ChefsController < ApplicationController
 
   def index
-    @chefs = Chef.page(params[:page])
+    sql = "SELECT r.id, r.name, r.chef_id, r.summary, r.created_at, COUNT(r.id) as recipes_count, 
+                                                                    COUNT(l.id) as likes_count, 
+                                                                    COUNT(d.id) as dislikes_count
+           FROM recipes AS r
+           LEFT JOIN chefs    as c ON r.chef_id   = c.id
+           LEFT JOIN likes    as l ON l.recipe_id = r.id
+           LEFT JOIN dislikes as d ON d.recipe_id = r.id
+           GROUP BY r.id"
+    @chefs = Kaminari.paginate_array(Chef.find_by_sql(sql)).page(params[:page])
   end
 
   def show
