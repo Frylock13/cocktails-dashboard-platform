376ae11d87cfb8ffef05af92a73b23ce83bb5878
Vitaliy <vitaliy.fry@gmail.com>
2016-04-06 01:38:36 +0300

    customize home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   
   def home
+    @recipes = Recipe.order('id DESC').limit(5)
+    @chefs = Chef.order('id DESC').limit(5)
+    @reviews = Review.order('id DESC').limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :recipes do
     post :like
     post :dislike
-    resources :reviews, except: [:index, :show]
+    resources :reviews, except: :show
   end
 
   resources :chefs, only: [:index, :show, :update]
