3cb819a12422ba12ecfdc3c07798fdeaa177f0a0
Vitaliy <vitaliy.fry@gmail.com>
2016-08-19 07:13:04 +0300

    add S3 for uploading images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,12 @@
 class Recipe < ApplicationRecord
 
-  has_attached_file :image, styles: { large: "500x500", default: Faker::Avatar.image }
+  has_attached_file :image, styles: { large: '500x500', thumb: '100x100' }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   validates :chef_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
-  validates :summary, presence: true, length: { minimum: 30, maximum: 300 }
-  validates :description, presence: true, length: { minimum: 300 }
+  #validates :summary, presence: true, length: { minimum: 30, maximum: 300 }
+  #validates :description, presence: true, length: { minimum: 100 }
 
   has_many :likes
   has_many :dislikes
