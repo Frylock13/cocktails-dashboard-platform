beb8958b1fa9b9d7cf2cf30822048b92f07257c1
Vitaliy <vitaliy.fry@gmail.com>
2016-08-21 02:10:59 +0300

    change env config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,14 +65,14 @@ Rails.application.configure do
     Bullet.alert = true
   end
 
-  # config.paperclip_defaults = {
-  #   storage: :s3,
-  #   s3_region: ENV["AWS_S3_REGION"],
-  #   s3_credentials: {
-  #     s3_host_name: ENV["AWS_S3_HOST_NAME"],
-  #     bucket: ENV["AWS_S3_BUCKET"],
-  #     access_key_id: ENV["AWS_S3_ACCESS_KEY_ID"],
-  #     secret_access_key: ENV["AWS_S3_SECRET_ACCESS_KEY"]
-  #   }
-  # } 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV["AWS_S3_REGION"],
+    s3_credentials: {
+      s3_host_name: ENV["AWS_S3_HOST_NAME"],
+      bucket: ENV["AWS_S3_BUCKET"],
+      access_key_id: ENV["AWS_S3_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_S3_SECRET_ACCESS_KEY"]
+    }
+  } 
 end
