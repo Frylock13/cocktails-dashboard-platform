90638f2496578be4f7764c20ab79134bf7c0bd6e
Vitaliy <vitaliy.fry@gmail.com>
2016-09-28 01:47:08 +0300

    remove callback

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,11 +1,5 @@
 class RecipeIngredient < ApplicationRecord
-
-  before_create :add_ingredient_to_ingredients_list
-
+  
   belongs_to :recipe
   belongs_to :ingredient, counter_cache: :recipes_count
-
-  def add_ingredient_to_ingredients_list
-    #self.
-  end
 end
--- db/migrate/20160927215510_add_column_ingredients_list_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnIngredientsListToRecipes < ActiveRecord::Migration[5.0]
-  def change
-    add_column :recipes, :ingredients_listss, :string
-  end
-end
